62551657718231f96347b2d8856767afc024a73c
DanteVasquezRivas <147457552+DanteVasquezRivas@users.noreply.github.com>
2024-07-07 17:54:56 -0400

    Agrega desafio soft jobs

---
+++ Front/src/config/constans.js
@@ -0,0 +1,8 @@
+export const URLBASE = 'http://localhost:3000/api/users'
+
+export const ENDPOINT = {
+  login: `${URLBASE}/login`,
+  users: `${URLBASE}/register`,
+  profile: `${URLBASE}/perfil`
+
+}
